35ab675937e488df47d3511e5ba7a411a596eafc
Alex Ionitel <oranggsoda@gmail.com>
2024-08-07 14:34:35 -0400

    drawing texture

--- src/main.c
+++ src/main.c
@@ -8,13 +8,22 @@ int main() {
     InitWindow(window_width, window_height, "Game");
     SetTargetFPS(60);
 
+    // Load texture.
+    Texture2D texture = LoadTexture("resources/walk.png");
+
     // Main game loop.
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        DrawFPS(10, 10);
+
+        // Draw texture to screen at position.
+        DrawTexture(texture, window_width/2 - texture.width/2, window_height/2 - texture.height/2, WHITE);
+
         EndDrawing();
     }
 
+    UnloadTexture(texture);
     CloseWindow();
     return 0;
 }
